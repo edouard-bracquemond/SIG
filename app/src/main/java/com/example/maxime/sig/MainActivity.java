ad515afabb53332cbb25fa480618fc1c47e59564
maximeNicot <maximenicot@outlook.com>
2019-11-07 20:36:28 +0100

    Web view

--- app/src/main/java/com/example/maxime/sig/MainActivity.java
+++ app/src/main/java/com/example/maxime/sig/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.maxime.sig;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        WebView myWebView = new WebView(this.getApplicationContext());
+        setContentView(myWebView);
+        myWebView.loadUrl("https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal");
     }
 }
