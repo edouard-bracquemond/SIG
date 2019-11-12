4827229ec72ef57d6c72e51868dad1e1fd01d648
maximeNicot <maximenicot@outlook.com>
2019-11-12 22:05:58 +0100

    Web view 2.0

--- app/src/main/java/com/example/maxime/sig/MainActivity.java
+++ app/src/main/java/com/example/maxime/sig/MainActivity.java
@@ -5,6 +5,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,10 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView myWebView = new WebView(this.getApplicationContext());
-        setContentView(myWebView);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webViewID);
+        myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal");
 
 
+
     }
+
 }
