09f1632ab6ad773ad5946408826320ac795fc31c
maximeNicot <maximenicot@outlook.com>
2019-11-16 15:06:18 +0100

    5 pages .js et leurs boutons

---
+++ app/src/main/assets/dechet.js
@@ -0,0 +1,28 @@
+ var osm_layer = new ol.layer.Tile({
+	source: new ol.source.OSM()
+});
+
+var wms_layer = new ol.layer.Tile({
+          source: new ol.source.TileWMS({
+            url: 'https://psigo.beta9.ovh/geoserver/wms',
+            params: {'LAYERS': 'sigo:dechets_pav', 'TILED': true},
+            serverType: 'geoserver',
+            transition: 0
+          })
+	});
+
+var layers = [
+	osm_layer,
+		wms_layer
+];
+
+
+
+ var map = new ol.Map({
+        target: 'map',
+        layers: layers,
+        view: new ol.View({
+          center: ol.proj.fromLonLat([1.927454, 47.836737]),
+          zoom: 11
+        })
+      });
--- app/src/main/java/com/example/maxime/sig/MainActivity.java
+++ app/src/main/java/com/example/maxime/sig/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,16 +17,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.webViewID);
+        final WebView myWebView = (WebView) findViewById(R.id.webViewID);
         myWebView.setWebViewClient(new WebViewClient());
-        //myWebView.loadUrl("https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal");
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.loadUrl("file:///android_asset/fondOpenLayer.html");
+        /*page qui load de base*/
+        myWebView.loadUrl("file:///android_asset/arbre.html");
 
-        Button bouton = (Button) findViewById(R.id.boutonTestID);
-        bouton.setText("Arbre");
+        Button boutonArbre = (Button) findViewById(R.id.boutonArbreID);
+        boutonArbre.setText("Arbre");
+        Button boutonCorbeille = (Button) findViewById(R.id.boutonCorbeilleID);
+        boutonCorbeille.setText("Corbeille");
+        Button boutonDechet = (Button) findViewById(R.id.boutonDechetID);
+        boutonDechet.setText("Dechet");
+        Button boutonSanitaire = (Button) findViewById(R.id.boutonSanitaireID);
+        boutonSanitaire.setText("Sanitaire");
+        Button boutonBanc = (Button) findViewById(R.id.boutonBancID);
+        boutonBanc.setText("Banc");
+
+
+
+        boutonArbre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("file:///android_asset/arbre.html");
+            }
+        });
+
+        boutonBanc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("file:///android_asset/banc.html");
+            }
+        });
+
+        boutonCorbeille.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("file:///android_asset/corbeille.html");
+            }
+        });
+
+        boutonDechet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("file:///android_asset/dechet.html");
+            }
+        });
+
+        boutonSanitaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWebView.loadUrl("file:///android_asset/sanitaire.html");
+            }
+        });
 
     }
+
 
 }
