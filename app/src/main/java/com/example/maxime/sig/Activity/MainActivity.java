8c979bc28ef93a7076340aad7c0640a207369c98
maximeNicot <maximenicot@outlook.com>
2019-11-25 19:48:59 +0100

    Photo depuis l'app, probleme depuis gallery

--- app/src/main/java/com/example/maxime/sig/Activity/MainActivity.java
+++ app/src/main/java/com/example/maxime/sig/Activity/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.maxime.sig.Activity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,10 +22,18 @@ import android.widget.Toast;
 import com.example.maxime.sig.GPS.GPSJScript;
 import com.example.maxime.sig.R;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
         Button boutonBanc = (Button) findViewById(R.id.boutonBancID);
         boutonBanc.setText("Banc");
 
+        Button boutonPhoto = (Button) findViewById(R.id.boutonPhotoID);
+        boutonPhoto.setText("Photo");
 
 
         boutonArbre.setOnClickListener(new View.OnClickListener() {
@@ -103,7 +118,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+        boutonPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+                galleryAddPic();
+            }
+        });
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {}
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
 }
